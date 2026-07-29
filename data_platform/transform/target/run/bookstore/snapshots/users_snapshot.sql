6b9fd34f10f88ffcb06a9cec4a8816e76a26ab0d
Er-shivam11 <shivamnirmal.097@gmail.com>
2026-07-29 11:40:53 +0530

    ran pipeline for new user

--- data_platform/transform/target/run/bookstore/snapshots/users_snapshot.sql
+++ data_platform/transform/target/run/bookstore/snapshots/users_snapshot.sql
@@ -1,46 +1,22 @@
 
-      
-  
-    
+      begin;
+    merge into "BOOKSTORE_DW"."SNAPSHOTS"."USERS_SNAPSHOT" as DBT_INTERNAL_DEST
+    using "BOOKSTORE_DW"."SNAPSHOTS"."USERS_SNAPSHOT__dbt_tmp" as DBT_INTERNAL_SOURCE
+    on DBT_INTERNAL_SOURCE.dbt_scd_id = DBT_INTERNAL_DEST.dbt_scd_id
 
-create or replace transient table BOOKSTORE_DW.SNAPSHOTS.users_snapshot
-    
-    
-    
-    as (
-    
+    when matched
+     
+       and DBT_INTERNAL_DEST.dbt_valid_to is null
+     
+     and DBT_INTERNAL_SOURCE.dbt_change_type in ('update', 'delete')
+        then update
+        set dbt_valid_to = DBT_INTERNAL_SOURCE.dbt_valid_to
 
-    select *,
-        md5(coalesce(cast(user_id as varchar ), '')
-         || '|' || coalesce(cast(load_timestamp as varchar ), '')
-        ) as dbt_scd_id,
-        load_timestamp as dbt_updated_at,
-        load_timestamp as dbt_valid_from,
-        
-  
-  coalesce(nullif(load_timestamp, load_timestamp), null)
-  as dbt_valid_to
-from (
-        
+    when not matched
+     and DBT_INTERNAL_SOURCE.dbt_change_type = 'insert'
+        then insert ("USER_ID", "EMAIL", "MOBILE", "IS_ACTIVE", "LOAD_TIMESTAMP", "DBT_UPDATED_AT", "DBT_VALID_FROM", "DBT_VALID_TO", "DBT_SCD_ID")
+        values ("USER_ID", "EMAIL", "MOBILE", "IS_ACTIVE", "LOAD_TIMESTAMP", "DBT_UPDATED_AT", "DBT_VALID_FROM", "DBT_VALID_TO", "DBT_SCD_ID")
 
-
-
-SELECT
-    user_id,
-    email,
-    mobile,
-    is_active,
-    load_timestamp
-
-FROM BOOKSTORE_DW.STAGING.stg_users
-
-    ) sbq
-
-
-
-    )
 ;
-
-
-  
+    commit;
   
