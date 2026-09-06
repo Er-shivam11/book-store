c96c34df77465b16ce5bf1ba3cf80c1a8b5fcd05
Er-shivam11 <shivamnirmal.097@gmail.com>
2026-09-07 00:21:58 +0530

    cleared

--- siyaram_bookstore/orders/views.py
+++ siyaram_bookstore/orders/views.py
@@ -20,6 +20,7 @@ class OrderViewSet(viewsets.ModelViewSet):
         if user.is_staff:
             return Order.objects.all().order_by("-created_at")
         return Order.objects.filter(user=user).order_by("-created_at")
+    # 
 
 
 class PaymentViewSet(viewsets.ModelViewSet):
