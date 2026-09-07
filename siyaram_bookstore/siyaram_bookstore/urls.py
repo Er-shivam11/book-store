d7dd3bec4db86c01518435b92128cc263ae8af84
Er-shivam11 <shivamnirmal.097@gmail.com>
2026-09-07 10:48:07 +0530

    updated image static

--- siyaram_bookstore/siyaram_bookstore/urls.py
+++ siyaram_bookstore/siyaram_bookstore/urls.py
@@ -31,6 +31,7 @@ urlpatterns = [
 ]
 
 # ⭐ Serve media files (PDF, images, etc.)
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
